7d87cb8ec572cb5f523c81039a7cf50db0419f9a
Subhash Naidu <Subhash@subhashs-imac.local>
2016-09-04 11:21:40 -0400

    Initial Commit

---
+++ ChessProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ChessProject
+//
+//  Created by subhash naidu on 9/4/16.
+//  Copyright © 2016 subhash naidu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
